058fe01301527e5a881a3c642addcb0871516017
Julia Rechkunova <julia.rechkunova@gmail.com>
2019-02-19 10:09:48 +0100

    Add GA tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,14 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-33234679-8",
+        head: false,
+        exclude: [],
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
